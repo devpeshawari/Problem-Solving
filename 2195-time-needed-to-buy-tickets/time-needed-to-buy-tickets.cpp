6ecc1a12cfa42010c9572e77e33b1f684fcb3138
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-04-09 15:13:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (67.56%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -2,28 +2,14 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int n = tickets.size();
-        queue<int> qu;
-
-        for(int i = 0; i < n; i++){
-            qu.push(i);
-        }
         int sol = 0;
-
-        while(!qu.empty()){
-            tickets[qu.front()]--;
-            sol++;
-            int temp = qu.front();
-            qu.pop();
-            if(tickets[temp] == 0){
-                if(temp == k){
-                    return sol;
-                }
-            }
-            else{
-                qu.push(temp);
-            }
+        sol+=tickets[k];
+        for(int i = 0; i < k; i++){
+            sol+= min(tickets[i],tickets[k]);
         }
-
+        for(int i = k + 1; i < n; i++){
+            sol+= min(tickets[i],tickets[k] - 1);
+        }
 
         return sol;
     }
