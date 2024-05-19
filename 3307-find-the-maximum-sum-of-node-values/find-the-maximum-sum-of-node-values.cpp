d3a8d5ae581964d952f7aa781724b4c35af0e069
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-05-19 12:31:08 +0530

    Time: 259 ms (15.78%) | Memory: 145.3 MB (20.89%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -1,34 +1,57 @@
 #define ll long long 
 class Solution {
 public:
-    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+    long long solve(int index, vector<int> &nums, int valid, int k, vector<vector<ll>> &dp){
         int n = nums.size();
-        ll sol = 0;
-
-        for(int i = 0; i < n; i++){
-            sol+= nums[i];
-        }
-        ll minIncrease = INT_MAX;
-        ll minDecrease = INT_MAX;
-        ll temp = 0;
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            ll xorDiff = (nums[i]^k) - nums[i];
-            if(xorDiff >= 0){
-                minIncrease = min(minIncrease,xorDiff);
-                temp+=xorDiff;
-                count++;
+        if(index >= n){
+            if(!valid){
+                return 0;
             }
             else{
-                minDecrease = min(minDecrease,abs(xorDiff));
+                return INT_MIN;
             }
         }
-
-
-        if(!(count & 1)){
-            return sol + temp;
+        if(dp[index][valid] != -1){
+            return dp[index][valid];
         }
+        ll take = (nums[index]^k) + solve(index + 1, nums, (valid + 1)%2, k, dp);
+        ll not_take = nums[index] + solve(index + 1, nums, valid%2, k, dp);
 
-        return max(sol + temp - minIncrease,sol + temp - minDecrease);
+
+        return dp[index][valid] = max(take,not_take);
+
+    }
+
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        int n = nums.size();
+        // ll sol = 0;
+
+        // for(int i = 0; i < n; i++){
+        //     sol+= nums[i];
+        // }
+        // ll minIncrease = INT_MAX;
+        // ll minDecrease = INT_MAX;
+        // ll temp = 0;
+        // int count = 0;
+        // for(int i = 0; i < n; i++){
+        //     ll xorDiff = (nums[i]^k) - nums[i];
+        //     if(xorDiff >= 0){
+        //         minIncrease = min(minIncrease,xorDiff);
+        //         temp+=xorDiff;
+        //         count++;
+        //     }
+        //     else{
+        //         minDecrease = min(minDecrease,abs(xorDiff));
+        //     }
+        // }
+
+
+        // if(!(count & 1)){
+        //     return sol + temp;
+        // }
+
+        // return max(sol + temp - minIncrease,sol + temp - minDecrease);
+        vector<vector<ll>> dp(n,vector<ll> (2,-1));
+        return solve(0,nums,0,k,dp);
     }
 };
