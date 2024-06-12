1afd8ab8a1e8f8ca4fbfc72a095a3bc324008f0a
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-06-12 17:01:14 +0530

    Time: 27 ms (60.03%) | Memory: 15.8 MB (96.14%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,59 +1,44 @@
+# define ll long long
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> sol;
         int n = nums.size();
-        if(n < 4){
-            return {};
-        }
-        int last = INT_MAX;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == last) continue;
-            int l = i + 1;
-            if(l >= n) continue;
-            while(l < n){
-                 int last1 = nums[l];
-                 int j = l + 1; 
-                 int k = n - 1;
-                while(j < k){
-                    long long sum = (long long)(nums[i]) + (long long)(nums[l]) + (long long)(nums[j]) + (long long)(nums[k]);
-                    if(sum == target){
-                        sol.push_back({nums[i],nums[l],nums[j],nums[k]});
-                        j++;
-                        while(j < n && nums[j] == nums[j -1]){
-                            j++;
-                        }
-                        k--;
-                        while(k >= 0 && nums[k] == nums[k + 1]){
-                            k--;
-                        }
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> sol;
+        for(int i = 0; i < n - 3; i++){
+            if((i != 0) && (nums[i] == nums[i - 1])){
+                continue;
+            }
+            for(int j = i + 1; j < n; j++){
+                if((j != i + 1) && (nums[j] == nums[j - 1])){
+                    continue;
+                }
+                int k = j + 1;
+                int l = n - 1;
+
+                while(k < l){
+                    long long diff = target - (ll)(nums[i]) - (ll)(nums[j]) - (ll)(nums[k]) - (ll)(nums[l]);
+                    
+                    if(diff == 0){
+                        sol.push_back({nums[i], nums[j], nums[k], nums[l]});
                     }
-                    else if(sum < target){
-                        j++;
-                        while(j < n && nums[j] == nums[j -1]){
-                            j++;
+
+                    if(diff > 0){
+                        k++;
+                        while(k < l && nums[k] == nums[k - 1]){
+                            k++;
                         }
                     }
                     else{
-                        k--;
-                        while(k >= 0 && nums[k] == nums[k + 1]){
-                            k--;
+                        l--;
+                        while(k < l && nums[l] == nums[l + 1]){
+                            l--;
                         }
                     }
-                    
                 }
-                l++;
-                while(l < n && nums[l] == last1){
-                    nums[l] = last1;
-                    l++; 
-                }
-
-                
             }
-
-            last = nums[i];
         }
+
         return sol;
     }
 };
