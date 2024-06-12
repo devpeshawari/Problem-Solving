cc945c00333d6b19c14385b7e8145f4f13cd0f01
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-06-12 10:19:03 +0530

    Time: 10 ms (54.33%) | Memory: 16.4 MB (54.99%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,39 +4,38 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         int temp = 1;
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
                     if(j == 0){
                         temp = 0;
-                        matrix[i][0] = 0;
-                        continue;
                     }
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
-                }
+                    else{
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }   
             }
         }
 
         for(int i = 1; i < n; i++){
             for(int j = 1; j < m; j++){
-                if((matrix[0][j] == 0) || (matrix[i][0] == 0)){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        for(int j = 0; j < m; j++){
-            if(matrix[0][0] == 0){
+        if(matrix[0][0] == 0){
+            for(int j = 0; j < m; j++){
                 matrix[0][j] = 0;
             }
         }
-        for(int i = 0; i < n; i++){
-            if(temp == 0){
+        if(temp == 0){
+            for(int i = 0; i < n; i++){
                 matrix[i][0] = 0;
             }
         }
-
-    
     }
 };
