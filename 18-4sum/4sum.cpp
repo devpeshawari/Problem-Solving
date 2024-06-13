c7f62c7e6a21e638542073126993d8ea02d3ff10
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-06-13 18:42:29 +0530

    Time: 40 ms (29.28%) | Memory: 16 MB (50.56%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,29 +1,29 @@
-# define ll long long
+#define ll long long
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         vector<vector<int>> sol;
-        for(int i = 0; i < n - 3; i++){
-            if((i != 0) && (nums[i] == nums[i - 1])){
+        for(int i = 0; i < n; i++){
+            if(i != 0 && nums[i] == nums[i - 1]){
                 continue;
             }
             for(int j = i + 1; j < n; j++){
-                if((j != i + 1) && (nums[j] == nums[j - 1])){
+                if(j != i + 1 && nums[j] == nums[j - 1]){
                     continue;
                 }
+
                 int k = j + 1;
                 int l = n - 1;
 
                 while(k < l){
-                    long long diff = target - (ll)(nums[i]) - (ll)(nums[j]) - (ll)(nums[k]) - (ll)(nums[l]);
-                    
+                    ll diff = (ll)(target) - (ll)(nums[i]) - (ll)(nums[j]) - (ll)(nums[k]) - (ll)(nums[l]);
                     if(diff == 0){
                         sol.push_back({nums[i], nums[j], nums[k], nums[l]});
                     }
 
-                    if(diff > 0){
+                    if(diff >= 0){
                         k++;
                         while(k < l && nums[k] == nums[k - 1]){
                             k++;
