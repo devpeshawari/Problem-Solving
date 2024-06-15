675e2766fb4f2878fdd8a5d019e801cba0e60df1
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-06-15 23:04:02 +0530

    Time: 57 ms (15.91%) | Memory: 46.8 MB (30.61%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -3,18 +3,27 @@ class Solution {
 public:
     ll solve(int i, int amount, vector<int> &coins, vector<vector<int>> &dp){
         int n = coins.size();
-        if(i >= n || amount < 0){
-            return INT_MAX;
-        }
-        if(dp[i][amount] != -1){
-            return dp[i][amount];
+        if(i == 0){
+            if(amount%coins[i] == 0){
+                return amount/coins[i];
+            }
+            else{
+                return INT_MAX;
+            }
         }
         if(amount == 0){
             return 0;
         }
+        if(dp[i][amount] != -1){
+            return dp[i][amount];
+        }
         
-        ll take = 1 + solve(i,amount - coins[i], coins, dp);
-        ll notTake = solve(i + 1,amount,coins,dp);
+        
+        ll take = INT_MAX;
+        if(amount >= coins[i]){
+            take = 1 + solve(i,amount - coins[i], coins, dp);
+        }
+        ll notTake = solve(i - 1,amount,coins,dp);
 
         
 
@@ -26,10 +35,13 @@ public:
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int> (amount + 1,-1));
 
-        int sol =  solve(0,amount,coins,dp);
+        int sol =  solve(n - 1,amount,coins,dp);
         if(sol == INT_MAX){
             return -1;
         }
         return sol;
+
+
+
     }
 };
