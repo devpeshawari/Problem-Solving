8da60c88dc62ab74d9554705d2ae55772f9fcee1
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-06-17 09:54:04 +0530

    Time: 35 ms (54.05%) | Memory: 79.6 MB (5.00%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,69 +1,56 @@
-#define ll long long
 class Solution {
 public:
-    ll solve(int i, int amount, vector<int> &coins, vector<vector<int>> &dp){
-        int n = coins.size();
+    int solve(int i, int amount, vector<int> &coins, vector<vector<int>> &dp){
+        if(amount == 0){
+            return 0;
+        }
         if(i == 0){
-            if(amount%coins[i] == 0){
-                return amount/coins[i];
+            if(amount%coins[0] == 0){
+                return amount/coins[0];
             }
             else{
                 return INT_MAX;
             }
         }
-        if(amount == 0){
-            return 0;
-        }
-        if(dp[i][amount] != -1){
-            return dp[i][amount];
-        }
-        
-        
-        ll take = INT_MAX;
+        int take = INT_MAX;
+        int notTake = solve(i - 1, amount, coins, dp);
         if(amount >= coins[i]){
-            take = 1 + solve(i,amount - coins[i], coins, dp);
+            take = 1 + solve(i, amount - coins[i], coins, dp);
         }
-        ll notTake = solve(i - 1,amount,coins,dp);
 
-        
-
-        return dp[i][amount] = min(take,notTake);
-;        
+        return min(take, notTake);
     }
-    
+
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        // vector<vector<ll>> dp(n, vector<ll> (amount + 1,INT_MAX));
-        // int sol =  solve(n - 1,amount,coins,dp);
-        // if(sol == INT_MAX){
-        //     return -1;
-        // }
-        // return sol;
-        vector<ll> last(amount + 1, INT_MAX), curr(amount + 1, INT_MAX);
-
+        vector<vector<long long>> dp(n,vector<long long> (amount + 1, INT_MAX));
+        
+        for(int i = 0; i < n; i++){
+            dp[i][0] = 0;
+        }
+        
         for(int i = 0; i <= amount; i++){
             if(i%coins[0] == 0){
-                last[i] = i/coins[0]; 
+                dp[0][i] = i/coins[0];
             }
         }
 
-        for(int i = 1; i < n; i++){
+        for(int i  = 1; i < n; i++){
             for(int j = 0; j <= amount; j++){
-                ll take = INT_MAX;
-                ll notTake = last[j];
+                long long take = INT_MAX;
+                long long notTake = dp[i - 1][j];
                 if(j >= coins[i]){
-                    take = 1 + curr[j - coins[i]];
+                    take = 1 + dp[i][j - coins[i]];
                 }
-                curr[j] = min(take,notTake);
+                dp[i][j] = min(take,notTake);
             }
-            last = curr;
         }
-        if(last[amount] == INT_MAX){
+
+        int sol = dp[n - 1][amount];
+
+        if(sol == INT_MAX){
             return -1;
         }
-
-        return last[amount];
-
-
+        return sol;
     }
 };
