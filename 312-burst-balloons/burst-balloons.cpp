704fac7d6511f260d4f38f3d1f89b5e9fad0626e
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-07-07 15:32:34 +0530

    Time: 214 ms (16.24%) | Memory: 12 MB (26.47%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -7,25 +7,22 @@ public:
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        int temp = INT_MIN;
+        int sol = INT_MIN;
         for(int k = i; k <= j; k++){
-            int score = nums[k]  * nums[i - 1] * nums[j + 1];
-            temp = max(temp,score + solve(i,k - 1,nums,dp) + solve(k + 1,j,nums,dp));
+            int add = nums[k] * nums[i - 1] * nums[j + 1];
+            sol = max(sol,add + solve(i,k - 1,nums,dp) + solve(k + 1, j, nums,dp));
         }
 
-        return dp[i][j] = temp;
+
+        return dp[i][j] = sol;
     }
     
     
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n + 1,vector<int>(n+ 1,-1));
-        vector<int> arr = {};
-        arr.push_back(1);
-        for(int i = 0; i < n; i++){
-            arr.push_back(nums[i]);
-        }
-        arr.push_back(1);
-        return solve(1,n,arr,dp);
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);  
+        vector<vector<int>> dp(n + 1,vector<int>(n + 1, -1));
+        return solve(1,n,nums,dp);
     }
-};
+}; 
