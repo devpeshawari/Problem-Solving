9520b19b6b8d44c769d6424764741b6dc72d47c3
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-07-13 13:05:59 +0530

    Time: 5 ms (11.72%) | Memory: 9.4 MB (13.29%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    int solve(int ind, vector<int> &nums, int k, vector<vector<int>> &dp){
-        if(k < 0){
-            return INT_MAX;
-        }
-        if(dp[ind][k] != -1){
-            return dp[ind][k];
-        }
+    bool cond(int m, int k, vector<int> &nums){
         int n = nums.size();
-        int sol = INT_MAX;
-        int sum = 0;
-        
-        for(int i = ind; i < n; i++){
-            sum+=nums[i];
-            int temp = INT_MAX;
-            if(i == n - 1){
-                if(k == 0){
-                    temp = sum;
-                    sol = min(temp,sol);
-                }
-                continue;
+        int split = 1;
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            total+=nums[i];
+            if(total > m){
+                total = nums[i];
+                split++;
             }
-            temp = max(sum, solve(i + 1,nums,k - 1,dp));
-            sol = min(temp,sol);
+        }
+        if(split <= k){
+            return true;
         }
 
-        
-
-        return dp[ind][k] = sol;
+        return false;
     }
     
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(k,-1));
-        return solve(0,nums,k - 1,dp);
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            total+=nums[i];
+        }
+        int l = *max_element(nums.begin(), nums.end());
+        int h = total;
+        int sol = h;
+        while(l <= h){
+            int m = (l + h)/2;
+            if(cond(m,k,nums)){
+                sol = m;
+                h = m - 1;
+            } 
+            else{
+                l = m + 1;
+            }
+        }
+
+        return sol;
     }
 };
