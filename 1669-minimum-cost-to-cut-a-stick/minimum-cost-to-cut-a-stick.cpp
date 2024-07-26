26e4a50109e8500ae26645f5f9253982bbace371
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-07-26 18:05:27 +0530

    Time: 37 ms (44.48%) | Memory: 12.2 MB (73.62%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int solve(int n, int i, int j, vector<int> &cuts, vector<vector<int>> &dp){
-        int m = cuts.size();
-        int start = 0;
-        int end = n;
+    int help(int i, int j, vector<int> &cuts, vector<vector<int>> &dp){
         if(i > j){
             return 0;
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        if(i > 0){
-            start = cuts[i - 1];
-        }
-        if(j < m - 1){
-            end = cuts[j + 1];
-        }
-        int temp = 1e9 + 7;
-        int length = end - start;
-        for(int k = i; k <= j; k++){
-            temp = min(temp, length + solve(n,i,k - 1,cuts, dp) + solve(n,k + 1,j,cuts,dp));
+        int sol = INT_MAX;
+        int length = cuts[j + 1] - cuts[i - 1];
+        for(int k = i; k <= j; k++){    
+            int temp = length + help(i,k - 1,cuts,dp) + help(k + 1,j,cuts, dp); 
+            sol = min(temp,sol);
         }
 
 
-        return dp[i][j] = temp;
+        return dp[i][j] = sol;
     }
+    
     
     int minCost(int n, vector<int>& cuts) {
         int m = cuts.size();
-        vector<vector<int>> dp(m,vector<int>(m, - 1));
+        vector<vector<int>> dp(m + 1,vector<int>(m + 1, - 1));
         sort(cuts.begin(), cuts.end());
-        return solve(n,0,m - 1,cuts, dp);
+        cuts.insert(cuts.begin(),0);
+        cuts.push_back(n);
+        return help(1,m,cuts,dp);
     }
 };
