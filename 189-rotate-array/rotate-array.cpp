5d893afcae9c0d2511fba540b6d6d9d83033c173
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-08-12 23:55:23 +0530

    Time: 24 ms (28.53%) | Memory: 29.1 MB (5.40%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
+    // [1 2 3 4 5] k = 2
+    // [4 5 1 2 3]
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
+        vector<int> first;
+        vector<int> second;
         k = k%n;
-        vector<int> sol(n);
-        for(int i = 0; i < n; i++){
-            sol[(i + k)%n] = nums[i];
-        } 
+        if(k == 0){
+            return;
+        }
+        for(int i = n - k; i < n; i++){
+            first.push_back(nums[i]);
+        }
 
-        for(int i = 0; i < n; i++){
-            nums[i] = sol[i];
+        for(int i = 0; i < n - k + 1; i++){
+            second.push_back(nums[i]);
+        }
+        
+        for(int i = 0; i < k; i++){
+            nums[i] = first[i];
+        }
+        for(int i = k; i < n; i++){
+            nums[i] =  second[i - k];
         }
     }
 };
