0e00cfcd0700b8dcbe6c15501b7528ee8f08f7cf
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2024-12-10 23:32:25 +0530

    Time: 0 ms (100.00%) | Memory: 34.1 MB (11.76%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> sol(n - k + 1, - 1);
+        vector<int> sol(n - k + 1,-1);
         int j = 0;
         for(int i = 0; i < n; i++){
             if(i != 0 && nums[i] != nums[i - 1] + 1){
                 j = i;
             }
             if(i - j + 1 == k){
-                sol[i - k + 1] = nums[i];
                 j++;
+                sol[i - k + 1] = nums[i];
             }
         }   
-
-
-        return sol; 
+        return sol;  
     }
 };
