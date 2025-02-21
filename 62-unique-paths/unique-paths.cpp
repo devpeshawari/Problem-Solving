0aedb0a68a5e3f2e0d02231c7f5881d0bb45bb74
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2025-02-21 11:13:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (8.99%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,49 +1,24 @@
 class Solution {
 public:
-    int solve(int x, int y, vector<vector<int>> &dp){
-        if(x == 0 && y == 0){
+    int solve(int m, int n, vector<vector<int>> &dp){
+        if(m == 0 && n == 0){
             return 1;
         }
-        if(dp[x][y] != -1){
-            return dp[x][y];
-        }
-        int up = 0;
         int left = 0;
-        if(x - 1 >= 0){
-            up = solve(x - 1,y,dp);
+        int right = 0;
+        if(dp[m][n] != -1) return dp[m][n];
+        if(m > 0){
+            left = solve(m - 1,n,dp);
         }
-        if(y - 1 >= 0){
-            left = solve(x,y - 1,dp);
+        if(n > 0){
+            right = solve(m,n - 1,dp);
         }
 
-        return dp[x][y] = left + up;
+        return dp[m][n] = left + right;
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        vector<int> last(n,0);
-        vector<int> curr(n,0);
-        last[0] = 1;
-        curr[0] = 1;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(i == 0 && j == 0){
-                    continue;
-                }    
-                int up = 0;
-                int left = 0;
-                if(i - 1 >= 0){
-                    up = last[j];
-                }
-                if(j - 1 >= 0){
-                    left = curr[j - 1];
-                }
-
-                curr[j] = left + up;
-            }
-            last = curr;
-        }
-
-        return last[n - 1];
+        vector<vector<int>> dp(m, vector<int>(n,-1));    
+        return solve(m - 1,n - 1,dp);
     }
 };
