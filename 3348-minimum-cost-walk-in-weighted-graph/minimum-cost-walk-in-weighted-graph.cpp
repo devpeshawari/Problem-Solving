bf534c7d1bb950498a2f64ceae6c194e6296fb2c
devpeshawari <69184747+devpeshawari@users.noreply.github.com>
2025-03-20 12:13:06 +0530

    Time: 27 ms (60.71%) | Memory: 204.7 MB (92.21%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -1,36 +1,36 @@
 class DisjointSet{
-public:
+public:    
+    vector<int> size;
     vector<int> parent;
-    vector<long long> size;
-
+    
     DisjointSet(int n){
         parent.resize(n);
         size.resize(n,1);
+
         for(int i = 0; i < n; i++){
             parent[i] = i;
         }
     }
 
     int findParent(int i){
-        if(parent[i] == i){
+        if(parent[i] == i){ 
             return i;
         }
 
         return parent[i] = findParent(parent[i]);
     }
 
-    void UnionBySize(int x, int y){
-        int ux = findParent(x);
-        int uy = findParent(y);
-        if(ux == uy){
-            return;
-        }
-        if(size[ux] >= size[uy]){
-            parent[uy] = ux;
+    void unionBySize(int x, int y){
+        int px = findParent(x);
+        int py = findParent(y);
+        if(px == py) return;
+        if(size[px] >= size[py]){
+            parent[py] = px;
+            size[px]+=size[py]; 
         }
         else{
-            parent[ux] = uy;
-            size[uy]+=size[ux];
+            parent[px] = py;
+            size[py]+=size[px];    
         }
     }
 
@@ -41,34 +41,32 @@ public:
 class Solution {
 public:
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        int m = edges.size();        
-        int q = query.size();
-        vector<int> sol(q);
+        unordered_map<int,int> mp;
+        int m = edges.size();
         DisjointSet ds(n);
-        vector<int> score(n,pow(2,31) - 1);
+        vector<int> sol;
         for(int i = 0; i < m; i++){
-            ds.UnionBySize(edges[i][0],edges[i][1]);
-        }
-        for(int i = 0; i < m; i++){
-            int parent = ds.findParent(edges[i][0]);
-            score[parent] = score[parent] & edges[i][2]; 
-        }
+            int node1 = edges[i][0];
+            int node2 = edges[i][1];
+            int weight = edges[i][2];
+            int parent1 = ds.findParent(node1);
+            int parent2 = ds.findParent(node2);
+            if(mp.find(parent1) != mp.end()) weight = weight & mp[parent1];
+            if(mp.find(parent2) != mp.end()) weight = weight & mp[parent2];
+            ds.unionBySize(parent1,parent2);
+            int parent = ds.findParent(parent1);
+            mp[parent] = weight;
+        }    
 
-        for(int i = 0; i < q; i++){
-            int u = query[i][0];
-            int v = query[i][1];
-            if(u == v){
-                sol[i] = 0;
-                continue;
-            }
-            if(ds.findParent(u) != ds.findParent(v)){
-                sol[i] = -1;
-            }
-            else{
-                sol[i] = score[ds.findParent(u)];
-            }
-        }
+        for(int i = 0; i < query.size(); i++){
+            int start = query[i][0];
+            int end = query[i][1];
+            int parent1 = ds.findParent(start);
+            int parent2 = ds.findParent(end);
 
+            if(parent1 == parent2) sol.push_back(mp[parent1]);
+            else sol.push_back(-1);
+        }
 
         return sol;
     }
